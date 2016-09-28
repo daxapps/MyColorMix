34b1633cc01e5fb645b5dc9615930431f4230362
daxapps <daxapps777@gmail.com>
2016-09-28 10:59:36 -0500

    add sliders and UI refinements

--- MyColorMix/ViewController.swift
+++ MyColorMix/ViewController.swift
@@ -11,55 +11,74 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
     
-    @IBAction func redSwitch(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
-        }
-        
-    }
-    @IBAction func greenSwitch(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .green
-        } else {
-            colorView.backgroundColor = .black
-        }
-    }
-    @IBAction func blueSwitch(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .blue
-        } else {
-            colorView.backgroundColor = .black
-        }
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+
+    @IBAction func switchChanged(_ sender: UISwitch) {
+//        if sender.isOn {
+//            colorView.backgroundColor = .red
+//        } else {
+//            colorView.backgroundColor = .black
+//        }
+        updateColor()
+        updateControls()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        colorView.backgroundColor = .black
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor = UIColor.black.cgColor
+        updateControls()
     }
-// It doesn't work b/c redSwitch is not a Bool
-//    func updateColor() {
-//        var red: CGFloat = 0
-//        var green: CGFloat = 0
-//        var blue: CGFloat = 0
-//        
-//        if redSwitch(UISwitch) {
-//            red = 1
-//        }
-//        if greenSwitch.isOn {
-//            green = 1
-//        }
-//        if blueSwitch.isOn {
-//            blue = 1
-//        }
-//        
-//        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
-//    }
-    
-   
 
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+        
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateColor()
+    }
+   
+    @IBAction func reset(_ sender: AnyObject) {
+        redSlider.value = 1
+        redSwitch.isOn = false
+        greenSlider.value = 1
+        greenSwitch.isOn = false
+        blueSlider.value = 1
+        blueSwitch.isOn = false
+        
+        updateColor()
+        updateControls()
+    }
+
+    func updateControls() {
+        redSlider.isEnabled = redSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+    }
 
 }
 
